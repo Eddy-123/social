4c0768ed112544ea64fccb7a31cebf66503d9ad8
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2024-09-03 17:37:37 +0100

    First unit test and url mapping

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,10 @@
+from django.urls import resolve
 from django.test import TestCase
+from social.settings import DEFAULT_AUTO_FIELD
+from .views import post_list
 
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, post_list)
