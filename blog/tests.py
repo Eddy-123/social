116c2cd6cc0b1e954b74add6639f9ae08f8d7e8f
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2024-09-04 07:18:12 +0100

    Post list view returns HTML

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
-from social.settings import DEFAULT_AUTO_FIELD
+from django.http import HttpRequest
 from .views import post_list
 
 
@@ -8,3 +8,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, post_list)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        html = response.content.decode("utf8")
+        html = html.strip()
+        self.assertTrue(html.startswith("<!DOCTYPE html>"))
+        self.assertIn("<title>My blog</title>", html)
+        self.assertTrue(html.endswith("</html>"))
